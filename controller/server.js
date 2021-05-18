d9954d42ac580ddad4e382d6019e2fa4c120e7b6
AmitaiYacobi <amitaiyacobi@gmail.com>
2021-05-18 19:51:33 +0300

    Added input creation

--- controller/server.js
+++ controller/server.js
@@ -3,6 +3,7 @@ const {get} = require('lodash');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 
@@ -24,16 +25,29 @@ app.get('/', (req, res) => {
   res.sendFile(index.html);
 });
 
+
 app.post('/detect', (req, res) => {
-  var algo
+  var algo;
   var algoIndex = req.body.algo;
   if (algoIndex == 1)
     algo = 'Regression';
   else if (algoIndex == 2)
     algo = 'Hybrid';
+  console.log(algo);
   if (req.files) {
     var trainCsv = req.files.train;
-    var testCSV = req.files.test;
-    console.log(trainCsv.data.toString());
+    var testCsv = req.files.test;
+    fs.writeFile('input.txt', trainCsv.data.toString(), (err) => {
+      if (err) throw err;
+    });
+    fs.appendFile('input.txt', 'done\n', (err) => {
+      if (err) throw err;
+    });
+    fs.appendFile('input.txt', testCsv.data.toString(), (err) => {
+      if (err) throw err;
+    });
+    fs.appendFile('input.txt', 'done', (err) => {
+      if (err) throw err;
+    });
   }
 });
