2bfcb96f4445d8544a948943be65bf7dd659c1f7
AmitaiYacobi <amitaiyacobi@gmail.com>
2021-05-19 18:22:21 +0300

    Added new model

--- model/anomalyDetection.js
+++ model/anomalyDetection.js
@@ -1,44 +1,34 @@
-const { Console } = require('console');
+const {Console} = require('console');
 const express = require('express');
 const fs = require('fs');
 
 const app = express();
+
 app.use(express.static('..'));
 
-function detect(algorithm, trainCSVpath, testCSVpath){
+function detect(algorithm, trainCsvPath, testCsvPath) {
+  trainCsv = fs.readFileSync(trainCsvPath).toString();
+  testCsv = fs.readFileSync(testCsvPath).toString();
   fs.writeFileSync('input.txt', algorithm, (err) => {
     if (err) throw err;
   });
   fs.appendFileSync('input.txt', '\ndone\n', (err) => {
     if (err) throw err;
   });
-  fs.readFile(trainCSVpath, (err,data)=>{
-      if(err)
-          console.log(err)
-      else {
-        fs.appendFile('input.txt', data.toString(), (err) => {
-          if (err) throw err;
-        });
-      }
-  })
+  fs.appendFileSync('input.txt', trainCsv, (err) => {
+    if (err) throw err;
+  });
   fs.appendFileSync('input.txt', 'done\n', (err) => {
     if (err) throw err;
   });
-  fs.readFile(testCSVpath, (err,data)=>{
-      if(err)
-          console.log(err)
-      else {
-        fs.appendFile('input.txt', data.toString(), (err) => {
-          if (err) throw err;
-        });
-      }
-  })
+  fs.appendFileSync('input.txt', testCsv, (err) => {
+    if (err) throw err;
+  });
   fs.appendFileSync('input.txt', 'done\n', (err) => {
     if (err) throw err;
   });
-      var result = "good" //call to c++ algorithm
-      return result; // to do: read from output.csv
+  var result = 'good'  // call to c++ algorithm
+  return result;       // to do: read from output.csv
 }
-
 
 module.exports.detect = detect
