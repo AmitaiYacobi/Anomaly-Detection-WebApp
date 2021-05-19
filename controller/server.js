ca9268328424541b5da85f6faf0a39602793d4d9
AmitaiYacobi <amitaiyacobi@gmail.com>
2021-05-19 18:16:59 +0300

    Added model and user story 2

--- controller/server.js
+++ controller/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-// const {get} = require('lodash');
-// const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const model = require('../model/anomalyDetection')
+const fs = require('fs');
 
 const app = express();
 
@@ -20,32 +18,37 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.json());
 
-// app.use(morgan('dev'));
-
 app.get('/', (req, res) => {
-  res.sendFile("./index.html");
+  res.sendFile('./index.html');
 });
 
-app.post('/detect/:algorithm/:trainCSV/:testCSV', (req, res)=>{
-  var result = model.detect(req.params.algorithm, req.params.trainCSV, req.params.testCSV);
+app.post('/detect/:algorithm/:trainCSV/:testCSV', (req, res) => {
+  var result = model.detect(
+      req.params.algorithm, req.params.trainCSV, req.params.testCSV);
   res.write(result);
   res.end();
 });
 
-
-
 app.post('/detect', (req, res) => {
   var algo;
   var algoIndex = req.body.algo;
-    if (algoIndex == 1)
-      algo = 'Regression';
-    else if (algoIndex == 2)
-      algo = 'Hybrid';
-  var fullPath = "/detect/"
+  if (algoIndex == 1)
+    algo = 'Regression';
+  else if (algoIndex == 2)
+    algo = 'Hybrid';
+  var fullPath = '/detect/'
+  var trainCsv = req.files.train.data.toString();
+  var testCsv = req.files.test.data.toString();
+  fs.writeFileSync('train.csv', trainCsv, (err) => {
+    if (err) throw err;
+  });
+  fs.writeFileSync('test.csv', testCsv, (err) => {
+    if (err) throw err;
+  });
   fullPath = fullPath.concat(algo)
-  fullPath = fullPath.concat("/")
-  fullPath = fullPath.concat(req.files.train.name)
-  fullPath = fullPath.concat("/")
-  fullPath = fullPath.concat(req.files.test.name)
+  fullPath = fullPath.concat('/')
+  fullPath = fullPath.concat('train.csv')
+  fullPath = fullPath.concat('/')
+  fullPath = fullPath.concat('test.csv')
   res.redirect(307, fullPath);
 });
